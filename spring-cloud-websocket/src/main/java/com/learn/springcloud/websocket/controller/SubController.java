3c1f330c9ca7b5777bac45a3e05f816a7baf27a2
shunzhong.deng <shunzhong.deng@gmail.com>
2018-11-06 11:57:32 +0800

    demo 运行成功

--- spring-cloud-websocket/src/main/java/com/learn/springcloud/websocket/controller/SubController.java
+++ spring-cloud-websocket/src/main/java/com/learn/springcloud/websocket/controller/SubController.java
@@ -9,14 +9,18 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class SubController {
     @Autowired
-    public SimpMessagingTemplate template;
+    public SimpMessagingTemplate simpMessagingTemplate;
 
 
+    /**
+     *  client 通过地址subscribe 发送rm， 并且需要订阅 /topic/getResponse（SimpleBroker）的消息
+     * @param rm
+     */
     @MessageMapping("/subscribe")
     public void subscribe(ReceiveMessage rm) {
         for (int i = 1; i <= 20; i++) {
             //广播使用convertAndSend方法，第一个参数为目的地，和js中订阅的目的地要一致
-            template.convertAndSend("/topic/getResponse", rm.getName());
+            simpMessagingTemplate.convertAndSend("/topic/getResponse", rm.getName());
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -26,13 +30,17 @@ public class SubController {
 
     }
 
+    /**
+     * client 通过地址 queue 发送rm， 并且需要订阅/user/zhangsan/message 才能获取消息
+     * @param rm
+     */
     @MessageMapping("/queue")
     public void queuw(ReceiveMessage rm) {
         System.out.println("进入方法");
         for (int i = 1; i <= 20; i++) {
             /*广播使用convertAndSendToUser方法，第一个参数为用户id，此时js中的订阅地址为
             "/user/" + 用户Id + "/message",其中"/user"是固定的*/
-            template.convertAndSendToUser("zhangsan", "/message", rm.getName());
+            simpMessagingTemplate.convertAndSendToUser("zhangsan", "/message", rm.getName());
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
